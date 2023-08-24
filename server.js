48a47e65fb9cc745e0f50bb8168620ad84637845
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-08-24 11:10:42 -0400

    server.js port additions

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+// make sure you use "npm i express" to download on the app
 const express = require("express");
 const api = require("./routes/index");
+const PORT = 3001;
 
 const app = express();
+
+app.listen(3000, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`),
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
