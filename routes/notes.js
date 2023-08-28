e71d0807f620dd1c399808e99265d75cb14bca51
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-08-27 22:20:32 -0400

    files organized correctly now, app is running but needs functionaility added on the /note api

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,3 @@
 const fb = require('express').Router();
+
+module.exports = fb;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) =>
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/assets/pages/notes.html'))
 );
 
 
