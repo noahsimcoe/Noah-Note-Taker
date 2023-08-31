4258a754d4dfb11f5eeedf494ce3a421e0a5b082
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-08-31 10:22:02 -0400

    added more comments on the server.js file

--- routes/notes.js
+++ routes/notes.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../public/helpers');
 
-// GET Route to retrieve all notes that are stored from db.json file
+// GET, retrieves all notes that are stored from db.json file
 router.get("/notes", (req, res) => (
     readFromFile("./db/db.json")).then((data) => res.json(JSON.parse(data)))
 );
 
-// POST Route to add new notes with randomly generated id's
+// POST, adds new notes with randomly generated id's
 router.post("/notes", (req, res) => {
     const { title, text } = req.body;
     if (title && text) {
@@ -28,7 +28,7 @@ router.post("/notes", (req, res) => {
     }
 });
 
-// DELETE Route to delete a specified note determined by its id
+// DELETE, deletes a specified note determined by its id
 router.delete("/notes/:id", (req, res) => {
     fs.readFile("db/db.json", "utf8", (err, data) => {
         const noteList = JSON.parse(data);
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // make sure you use "npm i express" to download on the app
+// Imports list
 const express = require("express");
 const path = require("path");
 const api = require("./routes/notes");
 const htmlRoutes = require("./routes/htmlroutes")
+
+// Specifies which port the Express.js server will run
 const PORT = process.env.PORT || 3001;
 
+// Initializes an isntance of Express.js
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use("/", htmlRoutes);
+
+// Invokes app.use() and serve static files from the '/public' folder
 app.use(express.static("public"));
 
 app.listen(PORT, () =>
