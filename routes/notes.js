97d73b0c4612b1ca69bea02c9847b82ae07ae812
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-08-31 10:16:35 -0400

    adding comments on the different routes

--- routes/notes.js
+++ routes/notes.js
@@ -2,12 +2,13 @@ const router = require("express").Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../public/helpers');
-const database = require("../db/db.json");
 
+// GET Route to retrieve all notes that are stored from db.json file
 router.get("/notes", (req, res) => (
     readFromFile("./db/db.json")).then((data) => res.json(JSON.parse(data)))
 );
 
+// POST Route to add new notes with randomly generated id's
 router.post("/notes", (req, res) => {
     const { title, text } = req.body;
     if (title && text) {
@@ -27,6 +28,7 @@ router.post("/notes", (req, res) => {
     }
 });
 
+// DELETE Route to delete a specified note determined by its id
 router.delete("/notes/:id", (req, res) => {
     fs.readFile("db/db.json", "utf8", (err, data) => {
         const noteList = JSON.parse(data);
